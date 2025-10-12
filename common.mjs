6b9749ca0a0d4a072f9360a4f4ad162433c98971
sheida-shab <shabankary@gmail.com>
2025-10-12 23:00:34 +0100

    Implement function getEventDate()  to get exact date of event

--- common.mjs
+++ common.mjs
@@ -3,3 +3,36 @@
 export function getGreeting() {
     return "Hello";
 }
+
+//Calculate exact day of the event based on the occurrence and dayname
+export function getEventDate(event, year, month) {
+  //Map occurrence to number
+  const occurrenceMap = {
+    first: 0,
+    second: 1,
+    third: 2,
+    fourth: 3,
+    last: -1,
+  };
+  const targetDayName = event.dayName;
+  const occurrenceIndex = occurrenceMap[event.occurrence];
+
+  const daysInThisMonth = dayjs().year(year).month(month).daysInMonth();
+  const matchingDays = []; //array of  exact date of dayname
+
+  //compare all days of the month with event's dayname
+  for (let d = 1; d <= daysInThisMonth; d++) {
+    const date = dayjs().year(year).month(month).day(d);
+    const dayName = date.format("dddd"); // like Monday,Tuesday,...
+    if (targetDayName === dayName) {
+      matchingDays.push(d);
+    }
+
+    //if occurrence is last return last item of array
+    if (occurrenceIndex === -1) {
+      return matchingDays[matchingDays.length - 1];
+    }
+    
+    return matchingDays[occurrenceIndex];
+  }
+}
