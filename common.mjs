d3f4b2f941b1180871329434a38df70838300e0f
Sara Tahir <saratahir34@gmail.com>
2025-10-15 11:29:34 +0100

    Clean up comments and debugging on html file.

--- common.mjs
+++ common.mjs
@@ -31,16 +31,16 @@ export function getEventDate(event, year, month) { //event from days.json, year 
     fourth: 3,
     last: -1
   };
-  const targetDayName = event.dayName; //e.g targetDayName becomes something like "Tuesday" and occurrence index becomes 1.
-  const occurrenceIndex = occurrenceMap[event.occurrence];//tell us e.g second tuesday of the month.
+  const targetDayName = event.dayName; //e.g targetDayName = "Tuesday" and occurrence index becomes 1.
+  const occurrenceIndex = occurrenceMap[event.occurrence];//e.g second tuesday of the month.
 
-  const daysInThisMonth = dayjs().year(year).month(month).daysInMonth(); //daysInThisMonth tells us how many days to loop over by taking the year and months passed into the function- selected by user in our calender
-  const matchingDays = []; //array of  exact dates of desired weekday
+  const daysInThisMonth = dayjs().year(year).month(month).daysInMonth(); //daysInThisMonth tells us how many days to loop over. 
+  const matchingDays = []; //[] of exact dates of desired weekday
 
   //compare all days of the month with event's dayname
   for (let d = 1; d <= daysInThisMonth; d++) {
-    const date = dayjs().year(year).month(month).date(d); //Calling dayjs() alone creates a date for today. passing year , month and date(d) sets it to the date we want.
-    const dayName = date.format("dddd"); // gives day of the week for the date
+    const date = dayjs().year(year).month(month).date(d); // Day.js object that represents a specific calendar date for every day
+    const dayName = date.format("dddd"); // gives day of the week for the date- string.
     if (targetDayName === dayName) {
       matchingDays.push(d);
     }
